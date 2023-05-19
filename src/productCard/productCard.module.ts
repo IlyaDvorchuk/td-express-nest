f2083594804cb67e88fc603444477353b9bfcc09
Tima Kolesnik <timakolesnik120@gmail.com>
2023-05-20 01:18:10 +0300

    настройка проекта

--- src/productCard/productCard.module.ts
+++ src/productCard/productCard.module.ts
@@ -6,13 +6,16 @@ import { ProductCard, ProductCardSchema } from './productCard.schema';
 import { ProductCardController } from './productCard.controller';
 import { ProductCardService } from './productCard.service';
 import { JwtStrategy } from '../utils/jwt.strategy';
+import { SheltersModule } from 'src/shelters/shelters.module';
+
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: ProductCard.name, schema: ProductCardSchema },
     ]),
-    PassportModule,
+    PassportModule,    
+    SheltersModule,
     JwtModule.register({
       secret: process.env.PRIVATE_KEY || 'SECRET',
       signOptions: {
--- src/productCard/productCard.service.ts
+++ src/productCard/productCard.service.ts
@@ -3,7 +3,6 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { ProductCard } from './productCard.schema';
 import { CreateProductCardDto } from './dto/create-product-card.dto';
-import { UploadedFile } from '@nestjs/common';
 import { join } from 'path';
 import { v4 as uuidv4 } from 'uuid';
 
