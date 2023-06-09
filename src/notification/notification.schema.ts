5cb4b04f66a1fa5e86428a41a96357248bdd2423
TimaKolesnik <tima.kolesnik.2015@mail.ru>
2023-06-09 14:40:57 +0300

    я хер знает почему не подхватилось

---
+++ src/notification/notification.schema.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { Document } from 'mongoose';
+
+export type NotificationDocument = Notification & Document;
+
+@Schema({ timestamps: true })
+export class Notification {
+  @Prop({ required: true })
+  userId: mongoose.Schema.Types.ObjectId;
+
+  @Prop({ required: true })
+  message: string;
+
+  @Prop({ default: false })
+  isRead: boolean;
+}
+
+export const NotificationSchema = SchemaFactory.createForClass(Notification);
