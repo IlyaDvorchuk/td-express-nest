a202c2ac6134d1612a8388b2c9cca9a314cb378c
John Doe <johndoe@example.com>
2023-06-16 20:06:49 +0300

    icon

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Post, Body } from "@nestjs/common";
 import { CategoriesService } from "./categories.service";
 
 @Controller('categories')
@@ -6,30 +6,30 @@ export class CategoriesController {
   constructor(private categoriesService: CategoriesService) {
   }
 
-  // @Post('/category')
-  // async saveCategory(@Body() dtoCat) {
-  //   return this.categoriesService.saveCategory(dtoCat)
-  // }
-  //
-  // @Post('/subcategories')
-  // async saveSubcategories(@Body() dtoCat) {
-  //   return this.categoriesService.saveSubcats(dtoCat)
-  // }
-  //
-  // @Post('/sections')
-  // async saveSections(@Body() dtoCat) {
-  //   return this.categoriesService.saveSections(dtoCat)
-  // }
-  //
-  // @Post('/ctosub')
-  // async cattoSub(@Body() dtoCat) {
-  //   return this.categoriesService.cattoSub(dtoCat)
-  // }
-  //
-  // @Post('/subtosec')
-  // async subtoSec(@Body() dtoCat) {
-  //   return this.categoriesService.subtoSec(dtoCat)
-  // }
+  @Post('/category')
+    async saveCategory(@Body() dtoCat) {
+    return this.categoriesService.saveCategory(dtoCat)
+  }
+  
+  @Post('/subcategories')
+  async saveSubcategories(@Body() dtoCat) {
+    return this.categoriesService.saveSubcats(dtoCat)
+  }
+  
+  @Post('/sections')
+  async saveSections(@Body() dtoCat) {
+    return this.categoriesService.saveSections(dtoCat)
+  }
+  
+  @Post('/ctosub')
+  async cattoSub(@Body() dtoCat) {
+    return this.categoriesService.cattoSub(dtoCat)
+  }
+  
+  @Post('/subtosec')
+  async subtoSec(@Body() dtoCat) {
+    return this.categoriesService.subtoSec(dtoCat)
+  }
 
   @Get()
   async getAllCategories() {
