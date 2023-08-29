0db768d8a46cfed4eeed493edee30d7bbc7f3163
Ilya Dvorchuk <iladvorchuk@gmail.com>
2023-08-29 08:48:42 +0300

    feat: добавлен поля заказу

--- src/order/order.schema.ts
+++ src/order/order.schema.ts
@@ -8,11 +8,17 @@ export class Order {
   @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'ProductCard', required: true })
   productId: string;
 
-  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true })
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'ProductCard', required: true })
+  typeId: string;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
   userId: string; 
 
   @Prop({ required: true })
   status: string;
+
+  @Prop({ required: true })
+  deliveryMethod: 'pickup' | 'express' | 'doorstep';
 }
 
 export const OrderSchema = SchemaFactory.createForClass(Order);
