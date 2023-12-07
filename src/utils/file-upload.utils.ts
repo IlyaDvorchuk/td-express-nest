e7b3d6169fad61f5267b7bf017ce962f7069fcdd
Ilya <dvorchukilya20@gmail.com>
2023-12-07 15:37:11 +0200

    fix: image i for imageFileFilter

--- src/utils/file-upload.utils.ts
+++ src/utils/file-upload.utils.ts
@@ -3,7 +3,7 @@ import { extname } from 'path';
 import * as uuid from 'uuid'
 
 export const imageFileFilter = (req, file, callback) => {
-  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
     return callback(
       new HttpException(
         'Only image files are allowed!',
